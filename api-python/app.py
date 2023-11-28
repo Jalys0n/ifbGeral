dd2d778e30eb128bee2d19032a8057c2ef5727cc
José Alyson da Costa Fontenele <jose.fontenele@intelit.com.br>
2023-11-28 13:28:14 -0300

    Continuação

--- api-python/app.py
+++ api-python/app.py
@@ -1,8 +1,8 @@
 from flask import Flask
-from controller import gerarSenhaNormal, gerarSenhaPreferencial
-from conexao import conexao, cadastrarAtendimento, cadastrarSenha, cadastrarAtendimento, cadastrarUsuario, listarAtendimento, listarSenhasNaFila   
+from controller import gerarSenhaNormal, cadastrarUsuario, cadastrarSenha, cadastrarAtendimento, listarAtendimento, listarSenhasNaFila, cadastrarNaTabelaSenha
 
 app = Flask(__name__)
+
 #página cadastro-usuario
 @app.route('/cadastro', methods = ['POST'])
 def cadastroUsuario():
@@ -16,18 +16,40 @@ def cadastroUsuario():
     else:
         senha = gerarSenhaNormal()
     cadastrarUsuario(cpf_usuario, nome, comunidade)
+    cadastrarSenha(senha)
+
+    #cadastrarAtendimento tem q ser por último, preciso rever esses parâmetros!
     cadastrarAtendimento()
 
-    return ("OIIII")
+    if cadastrarUsuario() and cadastrarSenha() and cadastrarAtendimento():
+        return "Usuário cadastrado com sucesso!"
+    else:
+        return "Cheque os dados e tente novamente! A senha não foi gerada"
+    
+
+
+
+
+
+#referentes ao servidor:
+
+@app.route('/loginServidor', methods = ['POST'])
+def loginServidor():
+    cpf_servidor = request.form.get['cpf_servidor']
+    senha_servidor = request.form.get['senha_servidor']
+    loginServidor(cpf_servidor, senha_servidor)
+
 
 @app.route('/chamarSenha', methods =['GET'])
 def chamarSenha():
     for n in listarSenhasNaFila():
         print(n)
-    return "Senha convocada!"
+
+    #fazer um if porque ai, quando estiver feita, mandar essa senha pra tabela senhas ok!
     
 
-
+    return "Senha convocada!"
+    
 
 
 
--- api-python/controller.py
+++ api-python/controller.py
@@ -1,5 +1,7 @@
 import random
 import string
+from conexao import conexao
+
 
 def gerarSenhaNormal(length=6):
     letras = 'ABC'
@@ -13,4 +15,49 @@ def gerarSenhaPreferencial(length=6):
    numeros = ''.join(random.choice(string.digits) for i in range(3))
    senha = letras + numeros
    return senha
+
+def cadastrarUsuario(cpf_usuario, nome):
+    sql = 'INSERT INTO usuario (cpf_usuario,nome, origem_usuario) values (?,?,?)'
+    cursor = conexao.cursor()
+    cursor.execute(sql,(cpf_usuario, nome))
+    conexao.commit()
+    cursor.close()
+
+def cadastrarSenha(idSenha):
+    sql = 'INSERT INTO emFila (idSenha) values (?)'
+    cursor = conexao.cursor()
+    cursor.execute(sql, (idSenha))
+    conexao.commit()
+    cursor.close() 
+
+def cadastrarAtendimento(guiche, fk_cpf_usuario, fk_cpf_servidor, fk_idSenha):
+   sql = 'INSERT INTO atendimento (guiche, fk_cpf_usuario, fk_cpf_servidor, fk_idSenha)'
+   cursor = conexao.cursor()
+   cursor.execute(sql, (guiche, fk_cpf_usuario, fk_cpf_servidor, fk_idSenha))
+   conexao.commit()
+   cursor.close()      
+
+def listarAtendimento():
+   sql = 'SELECT * FROM atendimento'
+   cursor = conexao.cursor()
+   cursor.execute(sql)
+   resultado = cursor.fetchall()
+   cursor.close()
+
+
+def listarSenhasNaFila():
+   sql = 'SELECT * FROM nafila'
+   cursor = conexao.cursor()
+   cursor.execute(sql)
+   resultado = cursor.fetchall()
+   cursor.close()
+
+
+def cadastrarNaTabelaSenha(idSenha):
+   sql = 'INSert into senhas (idSenha) values (?)'
+   cursor = conexao.cursor()
+   cursor.execute(sql)
+   conexao.commit()
+   cursor.close()
+#tabelas existentes no banco de dados: atendimento, senhas, servidores, tipo_atendimento, usuarios
 
